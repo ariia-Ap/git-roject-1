03e4bb5b99f6ad558aae5a605bbf3499d1b487a3
aolfazl <adibpourabolfazl@jmail.com>
2023-05-26 21:49:44 +0430

    commit to main
    my task

---
+++ git.2/my task/my task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            Console.WriteLine("please enter your name");
+          Name= Console.ReadLine();
+            Console.WriteLine("hello" + Name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ git.2/my task2/my task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num; 
+            Console.WriteLine("please enter your num");
+            string number = Console.ReadLine();
+            Num = int.Parse(number);
+            Num *= 3;
+            Console.WriteLine(Num);
+            Console.WriteLine("enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
